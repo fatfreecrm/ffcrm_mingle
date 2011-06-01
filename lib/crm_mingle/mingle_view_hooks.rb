6c0568efd9816f4acdfb05f2fcd3a95fbf7172a8
Ben Tillman <bstillman@crossroadsint.org>
2011-06-01 11:40:25 +0800

    Added mingle section to accounts and opportinities

--- lib/crm_mingle/mingle_view_hooks.rb
+++ lib/crm_mingle/mingle_view_hooks.rb
@@ -1,8 +1,21 @@
 class MingleViewHooks < FatFreeCRM::Callback::Base
 
   # --------------------------------------------------------------------
+  insert_before :show_account_bottom do |view, context|
+    view.render(:partial => "mingle/list", :locals => {:related => view.instance_variable_get(:@account)
+})
+  end
+
+  # --------------------------------------------------------------------
   insert_before :show_contact_bottom do |view, context|
-    view.render(:partial => "mingle/list")
+    view.render(:partial => "mingle/list", :locals => {:related => view.instance_variable_get(:@contact)
+})
+  end
+
+  # --------------------------------------------------------------------
+  insert_before :show_opportunity_bottom do |view, context|
+    view.render(:partial => "mingle/list", :locals => {:related => view.instance_variable_get(:@opportunity)
+})
   end
 
 end
