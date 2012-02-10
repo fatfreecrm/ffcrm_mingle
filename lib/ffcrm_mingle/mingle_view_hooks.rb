ba3613ca7d1002a8665203d9c4aca857553203b7
Ben Tillman <ben.tillman@gmail.com>
2012-02-10 16:14:57 +1300

    Removed view namespace

--- lib/ffcrm_mingle/mingle_view_hooks.rb
+++ lib/ffcrm_mingle/mingle_view_hooks.rb
@@ -2,19 +2,19 @@ class MingleViewHooks < FatFreeCRM::Callback::Base
 
   # --------------------------------------------------------------------
   insert_before :show_account_bottom do |view, context|
-    view.render(:partial => "fat_free_crm_mingle/mingle/list", :locals => {:related => view.instance_variable_get(:@account)
+    view.render(:partial => "mingle/list", :locals => {:related => view.instance_variable_get(:@account)
 })
   end
 
   # --------------------------------------------------------------------
   insert_before :show_contact_bottom do |view, context|
-    view.render(:partial => "fat_free_crm_mingle/mingle/list", :locals => {:related => view.instance_variable_get(:@contact)
+    view.render(:partial => "mingle/list", :locals => {:related => view.instance_variable_get(:@contact)
 })
   end
 
   # --------------------------------------------------------------------
   insert_before :show_opportunity_bottom do |view, context|
-    view.render(:partial => "fat_free_crm_mingle/mingle/list", :locals => {:related => view.instance_variable_get(:@opportunity)
+    view.render(:partial => "mingle/list", :locals => {:related => view.instance_variable_get(:@opportunity)
 })
   end
 
