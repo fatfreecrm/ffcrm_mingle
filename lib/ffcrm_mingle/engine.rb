ba37428e775dcef7f22d6bdea8f88fdbf5b193a8
Nathan Broadbent <nathan.f77@gmail.com>
2012-03-29 12:23:39 +0800

    Always show mingle settings tab in admin interface

--- lib/ffcrm_mingle/engine.rb
+++ lib/ffcrm_mingle/engine.rb
@@ -6,15 +6,16 @@ module FatFreeCRM
           puts "Please configure your mingle settings"
         else
           require 'ffcrm_mingle/mingle_view_hooks'
-          begin
-            FatFreeCRM::Tabs.admin << {
-              :text => "Mingle",
-              :url => { :controller => "admin/mingle" }
-            }
-          rescue TypeError
-            puts "You must migrate your settings table."
-          end
           ActionView::Base.send :include, MingleHelper
+        end
+
+        begin
+          FatFreeCRM::Tabs.admin << {
+            :text => "Mingle",
+            :url => { :controller => "admin/mingle" }
+          }
+        rescue TypeError
+          puts "You must migrate your settings table."
         end
       end
     end
